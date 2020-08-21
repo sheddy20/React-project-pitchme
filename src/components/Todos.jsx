c4342be0cbd4ba0d4fb9394feeb71c82e0f50618
Abel Shedrack Nicholas <shedrackejeh458@gmail.com>
2020-08-21 22:41:22 +0100

    change

---
+++ src/components/Todos.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Todos extends Component{
+    render(){
+        return(
+            <div>
+                <h1>hello worlds</h1>
+            </div>
+        );
+    }
+}
+
+export default Todos;
